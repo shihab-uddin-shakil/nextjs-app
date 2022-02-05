0b72315f1b0c877c7cf2ac11229db9d44629a28c
Shihab Uddin <62871586+shihab-uddin-shakil@users.noreply.github.com>
2022-02-05 19:43:35 +0600

    edit

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p></p>
           </a>
 
           <a
