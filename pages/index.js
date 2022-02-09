4a6647208f169e1699791cfb21a68be8033d51e4
Shihab Uddin <62871586+shihab-uddin-shakil@users.noreply.github.com>
2022-02-09 13:06:29 +0600

    name

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Shihab Uddin</a>
+          Welcome to DeepChainLabs <a href="https://nextjs.org">Md.Shihab Uddin</a>
         </h1>
 
         <p className={styles.description}>
